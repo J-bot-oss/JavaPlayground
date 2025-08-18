617b40b30b58b99b2cd9fcef0b4484737a9cac4f
J-bot-oss <jesseebong656@gmail.com>
2025-08-18 13:18:39 +0300

    Create HelloWorld.java
    This is our first program we can use to test whether the complier is working.

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+// This is a simple Java program that prints "Hello, World!" to the console
+public class HelloWorld { // this is the main class
+    public static void main(String[] args) { // this is the main method where execution starts
+        System.out.println("Hello, World!"); // this line prints "Hello, World!" to the console
+    }
+}
