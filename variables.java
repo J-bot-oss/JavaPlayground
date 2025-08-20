191384f2900023eacd029a5872a3624b6a0f1c79
J-bot-oss <jesseebong656@gmail.com>
2025-08-20 21:38:09 +0300

    Create variables.java
    This is variables part one for numbers.

---
+++ variables.java
@@ -0,0 +1,26 @@
+// so basically, quick reminder that variables are containers used to store data
+public class variables {
+    public static void main(String[] args) {
+        /* To initialize a variable in java;
+        we use variable_type variable_name = value:
+        */
+
+        // In Java numbers are represented either as int or double
+
+        int quantity = 5; // int is used for whole numbers
+        double item_Price = 2.50; // double sis used for decimal numbers
+
+        // To print tha values, we use System.out.println()
+        System.out.println("The quantity is:" + quantity); // The + operator is used to join strings and variables
+        // This will print the value of the quantity
+
+
+        System.out.println("The price of the item is:" + item_Price);
+        // This will print the value of the item_Price
+
+     
+
+        
+    }
+    
+}
