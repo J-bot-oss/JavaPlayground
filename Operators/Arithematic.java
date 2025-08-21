29256c2a68a19a446b0e5c885a37b750d94c159b
J-bot-oss <jesseebong656@gmail.com>
2025-08-21 14:04:07 +0300

    Create Arithematic.java
    Important points;
    Division truncates(/) decimal points for integers.
    Modulus(%) is used to check for odd or even numbers.

---
+++ Operators/Arithematic.java
@@ -0,0 +1,27 @@
+package Operators;
+
+public class Arithematic {
+    public static void main(String[] args){
+        // Arithematic operations in Java are performed using operators.
+        //The basic arithematic opertors are +, -, *, /, and %.
+
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b; // Addition
+        int difference = a - b; // Subtarction
+        int product = a * b; //Multiplication
+        int quotient = a / b; // Division
+        int remainder = a % b; // Modulus
+
+        // To print the results, we use System.out.println()
+        System.out.println("Sum:" + sum); // this will print the sum of a and b
+        System.out.println("Difference:" + difference); // this will print the difference between a and b
+        System.out.println("Product:" + product); // this will print the product of a and b
+        System.out.println("Quotient:" + quotient); // this will print the quotient of a and b
+        System.out.println("Remainder:" + remainder); // this will print the remainder of a divided by b
+        
+        
+    }
+    
+}
