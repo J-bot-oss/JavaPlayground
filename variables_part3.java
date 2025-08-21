fd30ed3d7d96c95aba4c165269c0e9a54985418e
J-bot-oss <jesseebong656@gmail.com>
2025-08-21 13:21:01 +0300

    Create variables_part3.java
    This is boolean variables

---
+++ variables_part3.java
@@ -0,0 +1,16 @@
+public class variables_part3 {
+    public static void main(String[] args) {
+        //In this part we are going to explore boolean variables
+        // Boolean variables are either true or false
+        boolean isAvailable = true;
+        boolean isSoldOut = false;
+
+        //To print the boolean values, we use System.out.println()
+        System.out.println("Is the item available:" + isAvailable); // This will print the value of isAvailable
+        System.out.println("Is the item sold out:" + isSoldOut); // This will print the value of isSoldOut
+
+}
+
+
+    
+}
